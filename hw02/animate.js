d2e5a7bd7409e839a39aaba5d3d300460f87dfd6
Caitlin Stanton <caitlinstanton44@gmail.com>
2016-02-23 22:31:13 -0500

    C

--- hw02/animate.js
+++ hw02/animate.js
@@ -1,8 +1,8 @@
 /*
-	Caitlin Stanton
-	SoftDev pd 3
-	HW 2 -- Circular
-	2016-02-23
+  Caitlin Stanton
+  SoftDev pd 3
+  HW 2 -- Circular
+  2016-02-22
 */
 
 var c = document.getElementById("playground");
@@ -11,42 +11,41 @@ var clear = document.getElementById("clear");
 
 var radius = 0;
 var growing = true;
-var startDraw = document.getElementById("start");
-var endDraw = document.getElementById("stop");
+var startButton = document.getElementById("start");
+var stopButton = document.getElementById("stop");
 
 var requestID;
 
 function drawDot() {
 
-  ctx.clearRect(0,0,c.width,c.height);
-  ctx.strokeRect(0,0,c.width,c.height);
+    ctx.clearRect(0,0,c.width,c.height);  //clears canvas
+    ctx.strokeRect(0,0,c.width,c.height);
 
-  if (growing == true) {
-    radius = radius + 1;
-  }
-  else {
-    radius = radius-1;
-  }
-	if (radius >= (c.width/2)) {
-		growing=false;
-  }
-	if (radius <= 0) {
-		growing=true;
-	}
-	ctx.beginPath();
-	ctx.arc(c.width/2, c.height/2, radius, 0, 2*Math.PI);
-	ctx.stroke();
-	ctx.fill();
+    if (growing == true) {
+	radius = radius + 1;
+    } else {
+	radius = radius - 1;
+    }
+    if (radius >= (c.width/2)) {
+	growing = false;
+    } else if (radius <= 0) {
+	growing = true;
+    }
 
-	requestID = window.requestAnimationFrame(drawDot);
+    ctx.beginPath();
+    ctx.arc(c.width/2, c.height/2, radius, 0, 2*Math.PI);
+    ctx.stroke();
+    ctx.fill();
+
+    requestID = window.requestAnimationFrame(drawDot);
 };
 
 function stop() {
-  ctx.clearRect(0,0,c.width,c.height); //clear canvas
-  growing = false;
-  radius = 0;
-  window.cancelAnimationFrame(requestID);
+    ctx.clearRect(0,0,c.width,c.height); //clears canvas
+    growing = false;
+    radius = 0;
+    window.cancelAnimationFrame(requestID);
 };
 
-startDraw.addEventListener("click", drawDot);
-endDraw.addEventListener("click", stop);
+startButton.addEventListener("click", drawDot);
+stopButton.addEventListener("click", stop);
