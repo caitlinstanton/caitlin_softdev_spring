5a8d74afdb30b302b8e281d871f540cc6d87c9d0
Your Name <caitlinstanton44@gmail.com>
2016-04-06 10:15:10 -0400

    rosetta code

--- sandbox/04-06/pythagorean.py
+++ sandbox/04-06/pythagorean.py
@@ -8,6 +8,10 @@ def pt(n):
     return triples
 
 def pt2(n):
-    return [(a,b,c) for a in range(1,n) for b in range(1,n) for c in range(1,n) if a*a + b*b == c*c]
+    return [(a,b,c) 
+		for a in range(1,n) 
+		for b in range(1,n) 
+		for c in range(1,n) 
+		if a*a + b*b == c*c]
 
 print pt2(15)
