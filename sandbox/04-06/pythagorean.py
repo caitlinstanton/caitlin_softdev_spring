918cf545fa4fb067e9113344806235f06b925dd0
Your Name <caitlinstanton44@gmail.com>
2016-04-06 09:59:48 -0400

    it's triplets!

---
+++ sandbox/04-06/pythagorean.py
@@ -0,0 +1,13 @@
+def pt(n):
+    triples = []
+    for a in range (1,n):
+        for b in range (1,n):
+            for c in range (1,n):
+                if (a*a + b*b == c*c):
+                    triples.append([a,b,c])
+    return triples
+
+def pt2(n):
+    return [(a,b,c) for a in range(1,n) for b in range(1,n) for c in range(1,n) if a*a + b*b == c*c]
+
+print pt2(15)
